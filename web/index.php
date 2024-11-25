d1acf2eb6dd46586cb63e1356d691d80c3fcfffa
anntoiiinee <antoine.lecarpentier@viacesi.fr>
2024-11-25 13:17:26 +0000

    feat(index.php): Added Devoteam favicon

--- web/index.php
+++ web/index.php
@@ -56,7 +56,9 @@
 ?>
 <html>
 <head>
-	<title>COGEDCT - Devoteam Cyber Trust</title>
+	<title>Cyber Indus</title>
+	<link rel="icon" href="https://gcloud.devoteam.com/wp-content/uploads/sites/32/2021/08/cropped-cropped-cropped-favicon-2-32x32.png" sizes="32x32">
+	<link rel="icon" href="https://gcloud.devoteam.com/wp-content/uploads/sites/32/2021/08/cropped-cropped-cropped-favicon-2-192x192.png" sizes="192x192">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
